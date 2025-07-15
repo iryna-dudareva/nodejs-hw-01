20dab83c6c0f3223a7c8ae6ee8957c9d1df2f3b5
iren <dudareva.iryna.study@gmail.com>
2025-07-15 21:56:18 +0200

    basic functionality

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,15 @@
-const generateContacts = async (number) => {};
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+import { createFakeContact } from "../utils/createFakeContact.js";
+
+
+const generateContacts = async (number) => {
+    const contacts = await readContacts();
+    const newContacts = Array.from({length : number},createFakeContact);
+    const updatedContacts = [...contacts, ...newContacts];
+    await writeContacts(updatedContacts);
+ };
 
 generateContacts(5);
+
+
